acfc736408933c2252c424d3daab731c70c0e636
Atharv Patil <patilathu24@gmail.com>
2025-11-08 14:47:05 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   preview: {
     host: true,
